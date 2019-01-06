9186e619ffa5fc0ef6d63bc2c1ec0c0063ff01c5
Byungjo-Ko <axc1922@gmail.com>
2019-01-06 17:23:03 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Ko's on 2019. 1. 6..
+//  Copyright © 2019년 GOByeongjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
